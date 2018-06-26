2695ff0151af6ef40beea8c01af06a322b18485d
DevAcademyFP <38556313+devacademyfp@users.noreply.github.com>
2018-06-26 22:20:19 +0200

    Add Class foe Dummy db + New proj for repository

---
+++ Foodall.Services/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Foodall.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foodall.Services
+{
+    public interface IRepository
+    {
+        //CRUD contract
+        IEnumerable<Member> GetMembers();
+        Member GetMember(int memberId);
+      
+        //TODO rest of the CRUD TBD
+
+    }
+}
---
+++ Foodall.Services/SqlRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Foodall.Data;
+using Foodall.Domain;
+
+namespace Foodall.Services
+{
+    public class SqlRepository : IRepository
+    {
+
+        private FoodallContext _context;
+
+        public SqlRepository(FoodallContext context)
+        {
+            _context = context;
+        }
+
+        public Member GetMember(int memberId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Member> GetMembers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
